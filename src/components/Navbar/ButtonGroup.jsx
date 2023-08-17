12020c770421c7149c98959216b1659797da3f9a
egileekada <egileoniso.ekada@gmail.com>
2023-08-17 16:23:25 +0100

    update on links

--- src/components/Navbar/ButtonGroup.jsx
+++ src/components/Navbar/ButtonGroup.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ButtonGroup = ({ white, blue, ctaText }) => {
+const ButtonGroup = ({ white, blue, ctaText, url }) => {
   const buttonClass = white
     ? "w-[147px] py-[14px] rounded-lg flex justify-center bg-white text-black border-[#D0D4EB] border-2"
     : blue
@@ -9,7 +9,7 @@ const ButtonGroup = ({ white, blue, ctaText }) => {
 
   return (
     <div className="flex gap-4">
-      <a className={buttonClass} href="#">
+      <a className={buttonClass} href={url}>
         {ctaText}
       </a>
     </div>
